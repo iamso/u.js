43e13d30ef962f67d536daef02b0240143977561
Steve <so@dev.so>
2015-02-02 16:17:29 +0100

    add prfx() method to get prefixed css property

--- src/u.js
+++ src/u.js
@@ -747,6 +747,21 @@
 
 
   /**
+   * prfx method
+   * get prefixed version of css properties
+   * @param  {string}    a     - css property
+   * @param  {undefined} b,c,d - placeholder variables
+   * @return {string}            prefixed css property
+   */
+  u.prfx = function prfx(a,b,c,d){
+    for (d?d=b.toUpperCase():b=4;!d&&b--;d=(d=d.replace(/-(.)/g,prfx)) in (new Image).style&&d) {
+      d=[['Moz-','Webkit-','Ms-','O-'][b]]+a;
+    }
+    return d;
+  },
+
+
+  /**
    * stop method
    * preventDefaut
    * @param  {object}    e - event
