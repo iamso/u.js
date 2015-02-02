cfccc9a5bdb57e58f55f79333d9260473b60e746
Steve <so@dev.so>
2015-02-02 16:18:30 +0100

    add tpl() method to parse template strings

--- src/u.js
+++ src/u.js
@@ -814,6 +814,26 @@
     }
   };
 
+  /**
+   * tpl method
+   * parse a template string with values
+   * from https://gist.github.com/haochi/1075080
+   * @param  {string} str - string containing {{variables}}
+   * @param  {object} obj - object containing values
+   * @return {string}       parsed string
+   */
+  u.tpl = function(str, obj){
+    return str.replace(/{{*([^}]+)*}}/g,
+      function(tmp, val){
+        tmp = obj;
+        val.replace(/[^.]+/g,function(key){
+          tmp = tmp[key];
+        });
+        return tmp;
+      }
+    );
+  },
+
 
   /**
    * ajax setup
