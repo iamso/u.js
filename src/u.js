8a32cc5876e94e8a8077a13a4a455a2f3d968e61
Steve <so@dev.so>
2015-04-22 10:23:43 +0200

    add parents method

--- src/u.js
+++ src/u.js
@@ -548,6 +548,35 @@
 
 
     /**
+     * parents method
+     * get the parent elements
+     * @return {object} element
+     */
+    parents: function(sel) {
+      var parents = [],
+          finished = false,
+          currentElement = this[0],
+          currentParent = false;
+
+      while (!finished) {
+        currentElement = currentElement.parentNode;
+        if (currentElement) {
+          if (sel === undef) {
+            parents.push(currentElement);
+          }
+          else if (u(currentElement).is(sel)) {
+            parents.push(currentElement);
+          }
+        }
+        else {
+          finished = true;
+        }
+      }
+      return parents;
+    },
+
+
+    /**
      * remove method
      * remove element from dom
      * @return {object} element
