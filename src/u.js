065146dfaf29fa62f3e3f5f1dc814342c8f9f9c8
Steve <so@dev.so>
2015-05-10 23:31:47 +0200

    add toHtml function

--- src/u.js
+++ src/u.js
@@ -878,6 +878,20 @@
 
 
   /**
+   * toHtml function
+   * convert an html string to DOM elements
+   * @param  {string} str   - string to be converted
+   * @param  {null}   [tmp] - placeholder for the temporary element
+   * @return {object}       - nodeList of the converted elements
+   */
+  u.toHtml = function(str, tmp) {
+    tmp = document.createElement('div');
+    tmp.innerHTML = str;
+    return str ? tmp.childNodes : [];
+  };
+
+
+  /**
    * bytes function
    * get byte size of a UTF-8 string
    * @param  {string} str - UTF-8 string
