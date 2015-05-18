678b75dc86e1839a3e5f5c64b8b7dcf6126e9f0d
Steve <so@dev.so>
2015-05-18 09:53:58 +0200

    add defer function

--- src/u.js
+++ src/u.js
@@ -1157,6 +1157,32 @@
 
 
   /**
+   * defer function
+   * a single "defer()" function that returns an object
+   * that is both a deferred and a thenable promise
+   * from https://gist.github.com/kirbysayshi/1129049
+   * @param  {function} [callbacks] - placeholder for pending callbacks
+   * @param  {*}        [value]     - placeholder for fulfilled value
+   * @return {object}               - the promise object
+   */
+  u.defer = function (callbacks, value) {
+    callbacks = [];
+    return {
+      resolve: function () {
+        value = arguments;
+        while (callbacks.length) {
+          callbacks.shift().apply({}, value);
+        }
+        callbacks = 0;
+      },
+      then: function (callback) {
+        callbacks ? callbacks.push(callback) : callback.apply({}, value);
+      }
+    };
+  };
+
+
+  /**
    * u session id
    * @type {string}
    */
