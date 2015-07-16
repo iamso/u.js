1cce615cf49020619562f6d23905fecfd6f98793
iamso <so@dev.so>
2015-07-16 11:40:20 +0200

    add index method

--- src/u.js
+++ src/u.js
@@ -497,6 +497,24 @@
 
 
     /**
+     * index method
+     * get the index of an element
+     * @param  {object|string} [el] - elements or css selector
+     * @return {number}               index
+     */
+    index: function(el) {
+      if (!el) {
+    		return this[0] ? this.first().prevAll().length : -1;
+    	}
+    	if (''+el === el) {
+    		return u.toArray(u(el)).indexOf(this[0]);
+    	}
+      el = el.ujs ? el[0] : el;
+    	return u.toArray(this).indexOf(el);
+    },
+
+
+    /**
      * prev method
      * get previous element sibling
      * @param  {string} [sel] - selector to filter siblings
