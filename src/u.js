6eb1507cf2f5a301815647d3f01c4ff4f6afc77a
iamso <so@dev.so>
2016-03-12 02:59:22 +0100

    new scrollTo method

--- src/u.js
+++ src/u.js
@@ -781,45 +781,41 @@
     /**
      * scrollTo method
      * scroll to a certain position inside the element
-     * @param  {number}   position - position to scroll to
+     * @param  {number}   to       - position to scroll to
      * @param  {number}   duration - duration for the animation
      * @param  {function} callback - function to call when finished
      * @return {object}   this
      */
-    scrollTo: function(position, duration, callback) {
-      var el = this[0],
-          _el = u(el),
-          _win = u(window),
-          winHeight = _win.height(),
-          scrollPos = _el.scrollTop(),
-          docHeight = el.scrollHeight,
-          startPosition = scrollPos,
-          newPosition = position,
-          maxPosition = docHeight - winHeight,
-          time = duration || 1500,
-          timeStep = 16,
-          limit = 3,
-          factor = Math.pow(limit / Math.abs(startPosition - newPosition), 1 / (time / timeStep));
+    scrollTo: function(to, duration, callback) {
+      return this.each(function(index, el) {
+        var _el = u(el),
+            start = _el.scrollTop(),
+            change = to - start,
+            currentTime = 0,
+            increment = 20;
+        duration = duration || 1500;
 
-      cancelAnimationFrame(el.animationId);
-      el.animationId = requestAnimationFrame(step);
-
-      function step (time) {
-          position = (maxPosition = docHeight - winHeight) < newPosition ? maxPosition : newPosition;
-          position = position < 0 ? 0 : position;
-
-          if ((Math.abs(scrollPos - position) > limit)) {
-            el.animationId = requestAnimationFrame(step);
-
-            scrollPos += (position - scrollPos) * (1 - factor);
-            _el.scrollTop(scrollPos);
+        function easing(t, b, c, d) {
+          t /= d/2;
+          if (t < 1) {
+            return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;
           }
-          else {
-            _el.scrollTop(position);
+          t--;
+          return c/2 * ( -Math.pow( 2, -10 * t) + 2 ) + b;
+        }
+
+        function animateScroll() {
+          currentTime += increment;
+          var val = easing(currentTime, start, change, duration);
+          _el.scrollTop(val);
+          if (currentTime < duration) {
+            requestAnimationFrame(animateScroll);
+          } else {
             callback && callback.apply(window);
           }
-      }
-      return this;
+        }
+        animateScroll();
+      });
     },
 
 
