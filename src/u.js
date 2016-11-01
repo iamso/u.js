79dbd02f5f21794d2d6e595cc346ba99f5603e93
iamso <so@dev.so>
2016-11-01 23:04:01 +0100

    use shorter polyfill function

--- src/u.js
+++ src/u.js
@@ -1314,19 +1314,10 @@
       if (!this.length) {
         return false;
       }
-      var m = (this[0].matches || this[0].matchesSelector || this[0].msMatchesSelector || this[0].mozMatchesSelector || this[0].webkitMatchesSelector || this[0].oMatchesSelector);
-      if (m) {
-        return m.call(this[0], sel);
-      }
-      else if (this[0].parentNode) {
-        var n = this[0].parentNode.querySelectorAll(sel);
-        for (var i = n.length; i--;) {
-          if (n[i] === this[0]) {
-            return true;
-          }
-        }
-      }
-      return false;
+      var m = (this[0].matches || this[0].matchesSelector || this[0].msMatchesSelector || this[0].mozMatchesSelector || this[0].webkitMatchesSelector || this[0].oMatchesSelector || function(s) {
+        return [].indexOf.call(document.querySelectorAll(s), this) !== -1;
+      });
+      return m.call(this[0], sel);
     },
 
 
